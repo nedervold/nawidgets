97ca849e8d75ce6cc5c01007d292897cc2ebbf1e
Eric Nedervold <nedervoldsoftware@gmail.com>
2020-05-19 11:04:30 -0700

    Reformatting.

--- src/main/java/org/nedervold/nawidgets/App.java
+++ src/main/java/org/nedervold/nawidgets/App.java
@@ -1,7 +1,7 @@
 package org.nedervold.nawidgets;
 
 public class App {
-    public static void main(String[] args){
-	System.out.println("Hello, world, from NaWidgets.");
-    }
+	public static void main(final String[] args) {
+		System.out.println("Hello, world, from NaWidgets!");
+	}
 }
